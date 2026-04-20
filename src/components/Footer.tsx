1a3d01a3036a69689c7e213de4d6621b12ab6ed9
khageshregion <m1997an@gmail.com>
2026-04-20 20:54:59 +0530

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center py-6">
+      <p>© 2026 Dr. Suharwardy</p>
+      <div className="mt-2">
+        <a href="https://www.facebook.com/drsuharwardy/" className="text-blue-400 hover:underline">
+          Facebook
+        </a>
+      </div>
+    </footer>
+  );
+}
